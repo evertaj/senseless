4248b6e31ba34e7bb58861884d9ea9651d3499aa
evertaj <overroy@mail.ru>
2016-12-15 16:42:34 +0100

    edit_puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,10 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
+
 environment ENV.fetch("RAILS_ENV") { "development" }
+
+bind 'unix:///home/evertaj.ru/productiom/shared/tmp/sockets/puma.sock'
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
