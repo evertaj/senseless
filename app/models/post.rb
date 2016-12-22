42ab67920443be090a34b2499e5cb8d62d1b5a9a
evertaj <overroy@mail.ru>
2016-12-22 15:22:12 +0100

    fix routes for approve

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   scope :most_recent, -> { order(created_at: :desc) }
 
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def should_generate_new_friendly_id?
     title_changed?
