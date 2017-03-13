cf418c0aacf2d67230ef47ee12a2ee20526deacf
evertaj <overroy@mail.ru>
2017-03-13 19:07:59 +0100

    fix upgraded likes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
     end
   end
 
+  def check_avatar(user, mode)
+    if user.avatars.first.nil? && mode == 'thumb'
+      return 'thumb_default_avatar.png'
+    elsif user.avatars.first.nil?
+      return 'default_avatar.png'
+    elsif mode == 'thumb'
+      return user.avatars.first.url(:thumb)
+    else
+      return user.avatars.first.url
+    end
+  end
+
 end
